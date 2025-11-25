2e58e3734176d495b66f9aa20b765cdbfc37d5b6
Van Tran FG <van.tran@fgorg.vn>
2025-11-25 08:34:38 +0700

    chỉnh lại route cho paging

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    // Enable image optimization
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  },
   vite: {
     plugins: [tailwindcss()]
   }
